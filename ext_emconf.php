612dcc21aaff13e8e3a722fa7e3cda3a3603aca5
dmitry <dmitry@735d13b6-9817-0410-8766-e36946ffe9aa>
2006-01-29 15:21:17 +0000

    Updated with latest changes
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/realurl/trunk@1123 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "realurl"
 #
-# Auto generated 28-01-2006 00:11
+# Auto generated 29-01-2006 17:20
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write
@@ -36,7 +36,7 @@ $EM_CONF[$_EXTKEY] = Array (
 	'private' => 0,
 	'download_password' => '',
 	'version' => '1.0.0',	// Don't modify this! Managed automatically during upload to repository.
-	'_md5_values_when_last_written' => 'a:30:{s:8:".project";s:4:"ca0e";s:9:"ChangeLog";s:4:"c6a5";s:10:"_.htaccess";s:4:"a6b1";s:20:"class.tx_realurl.php";s:4:"03fe";s:29:"class.tx_realurl_advanced.php";s:4:"6b81";s:26:"class.tx_realurl_dummy.php";s:4:"4dc6";s:33:"class.tx_realurl_userfunctest.php";s:4:"2cfe";s:12:"ext_icon.gif";s:4:"ea80";s:17:"ext_localconf.php";s:4:"6b61";s:14:"ext_tables.php";s:4:"0c3c";s:14:"ext_tables.sql";s:4:"b962";s:16:"locallang_db.php";s:4:"8dad";s:11:"CVS/Entries";s:4:"5e9e";s:14:"CVS/Repository";s:4:"57a9";s:8:"CVS/Root";s:4:"8fee";s:12:"doc/TODO.txt";s:4:"5419";s:14:"doc/manual.sxw";s:4:"4928";s:15:"doc/CVS/Entries";s:4:"095f";s:18:"doc/CVS/Repository";s:4:"3aab";s:12:"doc/CVS/Root";s:4:"8fee";s:38:"modfunc1/class.tx_realurl_modfunc1.php";s:4:"efec";s:22:"modfunc1/locallang.php";s:4:"984b";s:20:"modfunc1/CVS/Entries";s:4:"c55e";s:23:"modfunc1/CVS/Repository";s:4:"08b9";s:17:"modfunc1/CVS/Root";s:4:"8fee";s:16:"testmod/conf.php";s:4:"309a";s:17:"testmod/index.php";s:4:"b9ee";s:19:"testmod/CVS/Entries";s:4:"fce4";s:22:"testmod/CVS/Repository";s:4:"a207";s:16:"testmod/CVS/Root";s:4:"8fee";}',
+	'_md5_values_when_last_written' => 'a:33:{s:8:".project";s:4:"ca0e";s:9:"ChangeLog";s:4:"3cd0";s:10:"_.htaccess";s:4:"a6b1";s:20:"class.tx_realurl.php";s:4:"fd65";s:29:"class.tx_realurl_advanced.php";s:4:"e33b";s:26:"class.tx_realurl_dummy.php";s:4:"4dc6";s:33:"class.tx_realurl_userfunctest.php";s:4:"2cfe";s:12:"ext_icon.gif";s:4:"ea80";s:17:"ext_localconf.php";s:4:"6b61";s:14:"ext_tables.php";s:4:"0c3c";s:14:"ext_tables.sql";s:4:"b1af";s:16:"locallang_db.php";s:4:"8dad";s:11:"CVS/Entries";s:4:"e40a";s:14:"CVS/Repository";s:4:"57a9";s:8:"CVS/Root";s:4:"0741";s:12:"doc/TODO.txt";s:4:"5419";s:14:"doc/manual.sxw";s:4:"4928";s:15:"doc/CVS/Entries";s:4:"c40b";s:18:"doc/CVS/Repository";s:4:"3aab";s:12:"doc/CVS/Root";s:4:"0741";s:16:"mod1/CVS/Entries";s:4:"6cc4";s:19:"mod1/CVS/Repository";s:4:"3e6e";s:13:"mod1/CVS/Root";s:4:"0741";s:38:"modfunc1/class.tx_realurl_modfunc1.php";s:4:"3517";s:22:"modfunc1/locallang.php";s:4:"984b";s:20:"modfunc1/CVS/Entries";s:4:"fc43";s:23:"modfunc1/CVS/Repository";s:4:"08b9";s:17:"modfunc1/CVS/Root";s:4:"0741";s:16:"testmod/conf.php";s:4:"309a";s:17:"testmod/index.php";s:4:"b9ee";s:19:"testmod/CVS/Entries";s:4:"2813";s:22:"testmod/CVS/Repository";s:4:"a207";s:16:"testmod/CVS/Root";s:4:"0741";}',
 );
 
 ?>
