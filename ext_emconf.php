383e6c85de8563e46c7c48a07e4a6fa85ce64a79
dmitry <dmitry@735d13b6-9817-0410-8766-e36946ffe9aa>
2006-08-30 07:02:57 +0000

    Updated before releasing next version
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/realurl/trunk@3631 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,13 +3,14 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "realurl"
 #
-# Auto generated 29-01-2006 17:20
+# Auto generated 30-08-2006 09:56
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
 ########################################################################
 
-$EM_CONF[$_EXTKEY] = Array (
+$EM_CONF[$_EXTKEY] = array(
 	'title' => 'RealURL: URLs like normal websites',
 	'description' => 'Use the Path as shown in the Back-End in the URL, like: http://mydomain.com/en/products/product1/detailed_description/. Has some very neat extra features.',
 	'category' => 'fe',
@@ -18,8 +19,6 @@ $EM_CONF[$_EXTKEY] = Array (
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
-	'TYPO3_version' => '4.0.0-',
-	'PHP_version' => '-',
 	'module' => 'testmod',
 	'state' => 'beta',
 	'internal' => 0,
@@ -33,10 +32,20 @@ $EM_CONF[$_EXTKEY] = Array (
 	'author_company' => 'Beryllium Engineering',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'private' => 0,
-	'download_password' => '',
-	'version' => '1.0.0',	// Don't modify this! Managed automatically during upload to repository.
-	'_md5_values_when_last_written' => 'a:33:{s:8:".project";s:4:"ca0e";s:9:"ChangeLog";s:4:"3cd0";s:10:"_.htaccess";s:4:"a6b1";s:20:"class.tx_realurl.php";s:4:"fd65";s:29:"class.tx_realurl_advanced.php";s:4:"e33b";s:26:"class.tx_realurl_dummy.php";s:4:"4dc6";s:33:"class.tx_realurl_userfunctest.php";s:4:"2cfe";s:12:"ext_icon.gif";s:4:"ea80";s:17:"ext_localconf.php";s:4:"6b61";s:14:"ext_tables.php";s:4:"0c3c";s:14:"ext_tables.sql";s:4:"b1af";s:16:"locallang_db.php";s:4:"8dad";s:11:"CVS/Entries";s:4:"e40a";s:14:"CVS/Repository";s:4:"57a9";s:8:"CVS/Root";s:4:"0741";s:12:"doc/TODO.txt";s:4:"5419";s:14:"doc/manual.sxw";s:4:"4928";s:15:"doc/CVS/Entries";s:4:"c40b";s:18:"doc/CVS/Repository";s:4:"3aab";s:12:"doc/CVS/Root";s:4:"0741";s:16:"mod1/CVS/Entries";s:4:"6cc4";s:19:"mod1/CVS/Repository";s:4:"3e6e";s:13:"mod1/CVS/Root";s:4:"0741";s:38:"modfunc1/class.tx_realurl_modfunc1.php";s:4:"3517";s:22:"modfunc1/locallang.php";s:4:"984b";s:20:"modfunc1/CVS/Entries";s:4:"fc43";s:23:"modfunc1/CVS/Repository";s:4:"08b9";s:17:"modfunc1/CVS/Root";s:4:"0741";s:16:"testmod/conf.php";s:4:"309a";s:17:"testmod/index.php";s:4:"b9ee";s:19:"testmod/CVS/Entries";s:4:"2813";s:22:"testmod/CVS/Repository";s:4:"a207";s:16:"testmod/CVS/Root";s:4:"0741";}',
+	'version' => '1.0.0',
+	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"a2dd";s:10:"_.htaccess";s:4:"a6b1";s:20:"class.tx_realurl.php";s:4:"e258";s:29:"class.tx_realurl_advanced.php";s:4:"9835";s:26:"class.tx_realurl_dummy.php";s:4:"a824";s:33:"class.tx_realurl_userfunctest.php";s:4:"2cfe";s:12:"ext_icon.gif";s:4:"ea80";s:17:"ext_localconf.php";s:4:"6b61";s:14:"ext_tables.php";s:4:"0c3c";s:14:"ext_tables.sql";s:4:"1f61";s:16:"locallang_db.php";s:4:"b14f";s:12:"doc/TODO.txt";s:4:"5419";s:14:"doc/manual.sxw";s:4:"4928";s:38:"modfunc1/class.tx_realurl_modfunc1.php";s:4:"d011";s:22:"modfunc1/locallang.php";s:4:"984b";s:16:"testmod/conf.php";s:4:"309a";s:17:"testmod/index.php";s:4:"7990";}',
+	'constraints' => array(
+		'depends' => array(
+			'php' => '4.0.0-',
+			'typo3' => '4.0.0-',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
 );
 
 ?>
