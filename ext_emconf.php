afc7a0b5c9d8926e31ee03e550bead7476f7009d
dmitry <dmitry@735d13b6-9817-0410-8766-e36946ffe9aa>
2006-10-10 06:42:51 +0000

    Release of RealURL 1.1.3
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/realurl/trunk@3890 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "realurl"
 #
-# Auto generated 30-08-2006 09:56
+# Auto generated 10-10-2006 09:41
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -32,8 +32,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => 'Beryllium Engineering',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'version' => '1.1.2',
-	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"a2dd";s:10:"_.htaccess";s:4:"a6b1";s:20:"class.tx_realurl.php";s:4:"e258";s:29:"class.tx_realurl_advanced.php";s:4:"9835";s:26:"class.tx_realurl_dummy.php";s:4:"a824";s:33:"class.tx_realurl_userfunctest.php";s:4:"2cfe";s:12:"ext_icon.gif";s:4:"ea80";s:17:"ext_localconf.php";s:4:"6b61";s:14:"ext_tables.php";s:4:"0c3c";s:14:"ext_tables.sql";s:4:"1f61";s:16:"locallang_db.php";s:4:"b14f";s:12:"doc/TODO.txt";s:4:"5419";s:14:"doc/manual.sxw";s:4:"4928";s:38:"modfunc1/class.tx_realurl_modfunc1.php";s:4:"d011";s:22:"modfunc1/locallang.php";s:4:"984b";s:16:"testmod/conf.php";s:4:"309a";s:17:"testmod/index.php";s:4:"7990";}',
+	'version' => '1.1.3',
+	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"7a91";s:10:"_.htaccess";s:4:"a6b1";s:20:"class.tx_realurl.php";s:4:"455d";s:29:"class.tx_realurl_advanced.php";s:4:"4a99";s:26:"class.tx_realurl_dummy.php";s:4:"a824";s:33:"class.tx_realurl_userfunctest.php";s:4:"2cfe";s:12:"ext_icon.gif";s:4:"ea80";s:17:"ext_localconf.php";s:4:"6b61";s:14:"ext_tables.php";s:4:"0c3c";s:14:"ext_tables.sql";s:4:"46a0";s:16:"locallang_db.php";s:4:"b14f";s:12:"doc/TODO.txt";s:4:"5419";s:14:"doc/manual.sxw";s:4:"a14c";s:38:"modfunc1/class.tx_realurl_modfunc1.php";s:4:"aaec";s:22:"modfunc1/locallang.php";s:4:"984b";s:16:"testmod/conf.php";s:4:"309a";s:17:"testmod/index.php";s:4:"7990";}',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '4.0.0-',
