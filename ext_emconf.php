39d87e1b3c028bfdef377c521451e9805040695d
dmitry <dmitry@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-08-23 13:17:16 +0000

    Whitespace fix
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/realurl/trunk@6308 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "realurl"
 #
-# Auto generated 12-01-2007 16:48
+# Auto generated 24-01-2007 16:10
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -33,7 +33,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
 	'version' => '1.1.4',
-	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"5249";s:10:"_.htaccess";s:4:"a6b1";s:20:"class.tx_realurl.php";s:4:"1126";s:29:"class.tx_realurl_advanced.php";s:4:"3b71";s:26:"class.tx_realurl_dummy.php";s:4:"a824";s:33:"class.tx_realurl_userfunctest.php";s:4:"2cfe";s:12:"ext_icon.gif";s:4:"ea80";s:17:"ext_localconf.php";s:4:"7572";s:14:"ext_tables.php";s:4:"0c3c";s:14:"ext_tables.sql";s:4:"c7f1";s:16:"locallang_db.php";s:4:"b14f";s:12:"doc/TODO.txt";s:4:"5419";s:14:"doc/manual.sxw";s:4:"101c";s:38:"modfunc1/class.tx_realurl_modfunc1.php";s:4:"aaec";s:22:"modfunc1/locallang.php";s:4:"984b";s:16:"testmod/conf.php";s:4:"309a";s:17:"testmod/index.php";s:4:"7990";}',
+	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"09bd";s:10:"_.htaccess";s:4:"a6b1";s:20:"class.tx_realurl.php";s:4:"1126";s:29:"class.tx_realurl_advanced.php";s:4:"3b71";s:26:"class.tx_realurl_dummy.php";s:4:"a824";s:33:"class.tx_realurl_userfunctest.php";s:4:"2cfe";s:12:"ext_icon.gif";s:4:"ea80";s:17:"ext_localconf.php";s:4:"7572";s:14:"ext_tables.php";s:4:"0c3c";s:14:"ext_tables.sql";s:4:"c7f1";s:16:"locallang_db.php";s:4:"b14f";s:12:"doc/TODO.txt";s:4:"5419";s:14:"doc/manual.sxw";s:4:"8651";s:38:"modfunc1/class.tx_realurl_modfunc1.php";s:4:"aaec";s:22:"modfunc1/locallang.php";s:4:"984b";s:16:"testmod/conf.php";s:4:"309a";s:17:"testmod/index.php";s:4:"7990";}',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '4.0.0-',
