42d2b9f65d7250fd6e275bed439c4b377da68656
dmitry <dmitry@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-09-10 14:24:20 +0000

    More options and better descriptions
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/realurl/trunk@6470 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -19,16 +19,17 @@ if (is_array($_realurl_conf)) {
 	}
 	unset($_realurl_conf_file);
 }
-unset($_realurl_conf);
 
 // Autoconfiguration
 define('TX_REALURL_AUTOCONF_FILE', 'typo3temp/realurl_autoconf.php');
-if (!isset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']) && !@include_once(PATH_site . TX_REALURL_AUTOCONF_FILE)) {
+if ($_realurl_conf['enableAutoConf'] && !isset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']) && !@include_once(PATH_site . TX_REALURL_AUTOCONF_FILE)) {
 	require_once(t3lib_extMgm::extPath('realurl', 'class.tx_realurl_autoconfgen.php'));
 	$_realurl_gen = t3lib_div::makeInstance('tx_realurl_autoconfgen');
 	$_realurl_gen->generateConfiguration();
 	unset($_realurl_gen);
 	@include_once(PATH_site . TX_REALURL_AUTOCONF_FILE);
 }
+
+unset($_realurl_conf);
 
 ?>
