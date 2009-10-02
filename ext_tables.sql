2d1c1081f9c887ed477bdb4eeb214b54d1b05547
dmitry <dmitry@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-10-02 09:20:19 +0000

    Changed index bk_realurl02 (shorter key)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/realurl/trunk@25008 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.sql
+++ ext_tables.sql
@@ -32,7 +32,7 @@ CREATE TABLE tx_realurl_uniqalias (
 	PRIMARY KEY (uid),
 	KEY tablename (tablename),
 	KEY bk_realurl01 (field_alias,field_id,value_id,lang,expire),
-	KEY bk_realurl02 (tablename,field_alias,field_id,value_alias(220),expire)
+	KEY bk_realurl02 (tablename,field_alias,field_id,value_alias(20),expire)
 );
 
 #
