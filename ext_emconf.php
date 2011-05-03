9664b7bfaf7c3e725767ec54964611570fa9b54a
dmitry <dmitry@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-05-03 08:42:00 +0000

    Released RealURL 1.11.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/realurl/trunk@47243 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "realurl".
 #
-# Auto generated 31-01-2011 15:41
+# Auto generated 03-05-2011 11:40
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -32,12 +32,12 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => '',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'version' => '1.10.2',
-	'_md5_values_when_last_written' => 'a:19:{s:9:"ChangeLog";s:4:"3abd";s:10:"_.htaccess";s:4:"a6b1";s:20:"class.ext_update.php";s:4:"8518";s:20:"class.tx_realurl.php";s:4:"d5ad";s:29:"class.tx_realurl_advanced.php";s:4:"b3a8";s:32:"class.tx_realurl_autoconfgen.php";s:4:"5226";s:28:"class.tx_realurl_tcemain.php";s:4:"93e1";s:21:"ext_conf_template.txt";s:4:"c890";s:12:"ext_icon.gif";s:4:"ea80";s:17:"ext_localconf.php";s:4:"e297";s:14:"ext_tables.php";s:4:"e895";s:14:"ext_tables.sql";s:4:"cd65";s:17:"locallang_csh.xml";s:4:"369d";s:16:"locallang_db.xml";s:4:"5092";s:12:"doc/TODO.txt";s:4:"b8cb";s:14:"doc/manual.sxw";s:4:"74e5";s:38:"modfunc1/class.tx_realurl_modfunc1.php";s:4:"84ff";s:41:"modfunc1/class.tx_realurl_pagebrowser.php";s:4:"b616";s:22:"modfunc1/locallang.xml";s:4:"70b9";}',
+	'version' => '1.11.0',
+	'_md5_values_when_last_written' => 'a:19:{s:9:"ChangeLog";s:4:"f743";s:10:"_.htaccess";s:4:"a6b1";s:20:"class.ext_update.php";s:4:"8518";s:20:"class.tx_realurl.php";s:4:"0f61";s:29:"class.tx_realurl_advanced.php";s:4:"ef11";s:32:"class.tx_realurl_autoconfgen.php";s:4:"5226";s:28:"class.tx_realurl_tcemain.php";s:4:"93e1";s:21:"ext_conf_template.txt";s:4:"c890";s:12:"ext_icon.gif";s:4:"ea80";s:17:"ext_localconf.php";s:4:"e297";s:14:"ext_tables.php";s:4:"faed";s:14:"ext_tables.sql";s:4:"1b11";s:17:"locallang_csh.xml";s:4:"369d";s:16:"locallang_db.xml";s:4:"584a";s:12:"doc/TODO.txt";s:4:"b8cb";s:14:"doc/manual.sxw";s:4:"f0a2";s:38:"modfunc1/class.tx_realurl_modfunc1.php";s:4:"d1e5";s:41:"modfunc1/class.tx_realurl_pagebrowser.php";s:4:"b616";s:22:"modfunc1/locallang.xml";s:4:"7e4f";}',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.2.0-0.0.0',
-			'typo3' => '4.2.6-0.0.0',
+			'typo3' => '4.4.0-0.0.0',
 		),
 		'conflicts' => array(
 			'cooluri' => '',
