f8e157114d6009209626e87b9742e818eb30cf6a
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2013-03-13 12:26:05 +0400

    Released version 1.12.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "realurl".
 #
-# Auto generated 21-08-2012 19:13
+# Auto generated 13-03-2013 12:25
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -32,12 +32,12 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => '',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'version' => '1.12.3',
-	'_md5_values_when_last_written' => 'a:19:{s:9:"ChangeLog";s:4:"6d70";s:10:"_.htaccess";s:4:"a6b1";s:20:"class.ext_update.php";s:4:"8518";s:20:"class.tx_realurl.php";s:4:"85dc";s:29:"class.tx_realurl_advanced.php";s:4:"8dd0";s:32:"class.tx_realurl_autoconfgen.php";s:4:"5226";s:28:"class.tx_realurl_tcemain.php";s:4:"fb6f";s:16:"ext_autoload.php";s:4:"a6ff";s:21:"ext_conf_template.txt";s:4:"c890";s:12:"ext_icon.gif";s:4:"ea80";s:17:"ext_localconf.php";s:4:"e297";s:14:"ext_tables.php";s:4:"7182";s:14:"ext_tables.sql";s:4:"150c";s:17:"locallang_csh.xml";s:4:"369d";s:16:"locallang_db.xml";s:4:"584a";s:14:"doc/manual.sxw";s:4:"7bfb";s:38:"modfunc1/class.tx_realurl_modfunc1.php";s:4:"0b6f";s:41:"modfunc1/class.tx_realurl_pagebrowser.php";s:4:"b616";s:22:"modfunc1/locallang.xml";s:4:"7e4f";}',
+	'version' => '1.12.4',
+	'_md5_values_when_last_written' => 'a:19:{s:9:"ChangeLog";s:4:"3408";s:10:"_.htaccess";s:4:"9ab3";s:20:"class.ext_update.php";s:4:"9794";s:20:"class.tx_realurl.php";s:4:"0cea";s:29:"class.tx_realurl_advanced.php";s:4:"f387";s:32:"class.tx_realurl_autoconfgen.php";s:4:"f227";s:28:"class.tx_realurl_tcemain.php";s:4:"2983";s:16:"ext_autoload.php";s:4:"a6ff";s:21:"ext_conf_template.txt";s:4:"c890";s:12:"ext_icon.gif";s:4:"ea80";s:17:"ext_localconf.php";s:4:"e297";s:14:"ext_tables.php";s:4:"7182";s:14:"ext_tables.sql";s:4:"150c";s:17:"locallang_csh.xml";s:4:"369d";s:16:"locallang_db.xml";s:4:"584a";s:14:"doc/manual.sxw";s:4:"3b3d";s:38:"modfunc1/class.tx_realurl_modfunc1.php";s:4:"d3a6";s:41:"modfunc1/class.tx_realurl_pagebrowser.php";s:4:"60b1";s:22:"modfunc1/locallang.xml";s:4:"7e4f";}',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.2.0-0.0.0',
-			'typo3' => '4.4.0-6.0.99',
+			'php' => '5.3.2-5.999.999',
+			'typo3' => '4.5.0-6.1.999',
 		),
 		'conflicts' => array(
 			'cooluri' => '',
