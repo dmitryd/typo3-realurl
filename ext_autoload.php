a52257a56b0cdb17fd3fbda91ab4c75bd3cad33c
Johann Allgaeuer <h.allgaeuer@hotmail.de>
2013-08-28 18:45:34 +0200

    [BUGFIX] RealUrl: Fixing fatal error in info module in TYPO3 6.2
    RealUrls Info -> RealUrl Management module throws an fatal error because
    of an class it cannot find. This patch adds the class to the
    ext_autoload.php and fixes this error.
    
    Resolves: #51471
    Releases: 6.2
    Change-Id: I94f3a3e588a4ccf0ec02c42f0b73948ac877de77
    Reviewed-on: https://review.typo3.org/23430
    Reviewed-by: Stefan Fürst
    Tested-by: Stefan Fürst
    Reviewed-by: Kai Ole Hartwig
    Tested-by: Kai Ole Hartwig
    Reviewed-by: Dmitry Dulepov
    Tested-by: Dmitry Dulepov

--- ext_autoload.php
+++ ext_autoload.php
@@ -1,5 +1,6 @@
 <?php
 $extpath = t3lib_extMgm::extPath('realurl');
 return array(
-   'tx_realurl' => $extpath . 'class.tx_realurl.php'
+   'tx_realurl' => $extpath . 'class.tx_realurl.php',
+   'tx_realurl_modfunc1' => $extpath . 'modfunc1/class.tx_realurl_modfunc1.php'
 );
