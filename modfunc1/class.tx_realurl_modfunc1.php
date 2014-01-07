2b7a9eb7b82074373db153027af7993efc63f4ae
Simon Schaufelberger <simonschaufi@googlemail.com>
2014-01-07 22:53:52 +0100

    [FEATURE] Don't loose page in pager
    The page is carried along while editing a record in the redirects table
    
    Change-Id: Id3f33a70aaa1329b5186d5ca4028dd1f76fca760
    Resolves: #54822
    Reviewed-on: https://review.typo3.org/26685
    Reviewed-by: Dmitry Dulepov
    Tested-by: Dmitry Dulepov

--- modfunc1/class.tx_realurl_modfunc1.php
+++ modfunc1/class.tx_realurl_modfunc1.php
@@ -1548,7 +1548,7 @@ class tx_realurl_modfunc1 extends t3lib_extobjbase {
 		$output = '';
 		while (false !== ($rec = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res))) {
 			$output .= '<tr class="bgColor'.($itemCounter%2 ? '-20':'-10').'">' .
-				$this->generateSingleRedirectContent($rec);
+				$this->generateSingleRedirectContent($rec, $page);
 			$itemCounter++;
 		}
 		$GLOBALS['TYPO3_DB']->sql_free_result($res);
@@ -1586,14 +1586,15 @@ class tx_realurl_modfunc1 extends t3lib_extobjbase {
 	 * Creates an HTML table row for a single redirect record.
 	 *
 	 * @param array $rec
+	 * @param int $page
 	 * @return string
 	 */
-	protected function generateSingleRedirectContent(array $rec) {
+	protected function generateSingleRedirectContent(array $rec, $page) {
 		$output = '<td>'.
-					'<a href="'.$this->linkSelf('&cmd=edit&uid=' . rawurlencode($rec['uid'])) .'">'.
+					'<a href="'.$this->linkSelf('&cmd=edit&uid=' . rawurlencode($rec['uid'])) . '&page='.$page.'">'.
 					'<img'.t3lib_iconWorks::skinImg($this->pObj->doc->backPath,'gfx/edit2.gif','width="11" height="12"').' title="Edit entry" alt="" />'.
 					'</a>'.
-					'<a href="'.$this->linkSelf('&cmd=delete&uid=' . rawurlencode($rec['uid'])) . '">'.
+					'<a href="'.$this->linkSelf('&cmd=delete&uid=' . rawurlencode($rec['uid'])) . '&page='.$page.'">'.
 					'<img'.t3lib_iconWorks::skinImg($this->pObj->doc->backPath,'gfx/garbage.gif','width="11" height="12"').' title="Delete entry" alt="" />'.
 					'</a>'.
 				'</td>';
@@ -1797,6 +1798,7 @@ class tx_realurl_modfunc1 extends t3lib_extobjbase {
 			'url,url_hash,destination,has_moved,domain_limit', 'tx_realurl_redirects',
 			'uid=' . intval($uid));
 		if (is_array($row)) {
+			$page = max(1, intval(t3lib_div::_GP('page')));
 			$content = '<table border="0" cellspacing="2" cellpadding="1" style="margin-bottom:1em">' .
 				'<tr><td>Redirect from:</td>' .
 				'<td width="1">/</td><td><input type="text" name="data[0][source]" value="' . htmlspecialchars($row['url']) . '" size="40" /></td></tr>' .
@@ -1809,7 +1811,8 @@ class tx_realurl_modfunc1 extends t3lib_extobjbase {
 				'<tr><td colspan="2"></td><td>' . $this->saveCancelButtons() . '</td></tr>' .
 				'</table>' .
 				'<input type="hidden" name="data[0][uid]" value="' . intval($uid) . '" />' .
-				'<input type="hidden" name="data[0][url_hash]" value="' . $row['url_hash'] . '" />'
+				'<input type="hidden" name="data[0][url_hash]" value="' . $row['url_hash'] . '" />' .
+				'<input type="hidden" name="page" value="' . intval($page) . '" />'
 				;
 		}
 		return $content;
