a34ec3fb1acd84a95a110e85c47cb1505b32f301
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2015-11-30 17:45:32 +0300

    [TASK] Adjust version dependencies
    Change-Id: I26a16a4819a1cc3bc5eca7420e7b4c31042d6eb6

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,6 +16,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '6.2.0-7.99.99',
+			'php' => '5.3.2-'
 		),
 		'conflicts' => array(
 			'cooluri' => ''
