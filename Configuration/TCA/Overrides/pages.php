164f5118e5390b451455ae53ef54ccd4f092b39e
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2016-02-05 16:56:46 +0300

    [TASK] Move overrides for pages_language_overlay into its proper file
    Change-Id: I98b35c5c9072b916c08b6280e9146232f69b17b4

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -54,17 +54,4 @@ if (!isset($GLOBALS['TCA']['pages']['columns']['tx_realurl_pathsegment'])) {
 		'showitem' => 'tx_realurl_pathsegment,--linebreak--,tx_realurl_exclude,tx_realurl_pathoverride'
 	);
 
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages_language_overlay', array(
-		'tx_realurl_pathsegment' => array(
-			'label' => 'LLL:EXT:realurl/Resources/Private/Language/locallang_db.xlf:pages.tx_realurl_pathsegment',
-			'exclude' => 1,
-			'config' => array (
-				'type' => 'input',
-				'max' => 255,
-				'eval' => 'trim,nospace,lower'
-			),
-		),
-	));
-
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('pages_language_overlay', 'tx_realurl_pathsegment', '', 'after:nav_title');
 }
