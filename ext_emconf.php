4d48b25b1837c6c4e43a493a20eed5ae9d95f1b5
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2016-03-03 11:03:15 +0300

    [TASK] Update ext_emconf.php before the release
    Change-Id: I36a768db3b75dd4b54603b9afb5bfc3cd81d097d

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,24 +2,25 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Speaking URLs for TYPO3',
-	'description' => '',
+	'description' => 'Makes TYPO3 URLs search egnine friendly. Donations are welcome to dmitry.dulepov@gmail.com. They help to support the extension!',
 	'category' => 'services',
 	'version' => '2.0.0',
-	'state' => 'alpha',
+	'state' => 'stable',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => 'pages,sys_domain,pages_language_overlay,sys_template',
 	'clearcacheonload' => 1,
 	'author' => 'Dmitry Dulepov',
 	'author_email' => 'dmitry.dulepov@gmail.com',
-	'author_company' => 'SIA ACCIO',
+	'author_company' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.99.99',
+			'typo3' => '6.2.0-7.0.99',
 			'php' => '5.3.2-'
 		),
 		'conflicts' => array(
-			'cooluri' => ''
+			'cooluri' => '',
+			'simulatestatic' => ''
 		),
 		'suggests' => array(
 			'static_info_tables' => '6.2.0-',
