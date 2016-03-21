a719c7f80d1989196af4530408b63d44feffd875
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2016-03-21 11:07:20 +0300

    [BUGFIX] Fix #90: URLs are not encoded from scheduler jobs

--- Classes/Encoder/UrlEncoder.php
+++ Classes/Encoder/UrlEncoder.php
@@ -216,7 +216,7 @@ class UrlEncoder extends EncodeDecoderBase {
 	 * @return bool
 	 */
 	protected function canEncoderExecute() {
-		return $this->isRealURLEnabled() && !$this->isBackendMode() && !$this->isInWorkspace() && $this->isTypo3Url() && $this->isProperTsfe();
+		return $this->isRealURLEnabled() && !$this->isInWorkspace() && $this->isTypo3Url() && $this->isProperTsfe();
 	}
 
 	/**
@@ -943,15 +943,6 @@ class UrlEncoder extends EncodeDecoderBase {
 	protected function initializeConfiguration() {
 		$this->configuration = GeneralUtility::makeInstance('DmitryDulepov\\Realurl\\Configuration\\ConfigurationReader', ConfigurationReader::MODE_ENCODE, $this->urlParameters);
 		$this->configuration->validate();
-	}
-
-	/**
-	 * Checks if system runs in non-live workspace
-	 *
-	 * @return boolean
-	 */
-	protected function isBackendMode() {
-		return (TYPO3_MODE == 'BE');
 	}
 
 	/**
