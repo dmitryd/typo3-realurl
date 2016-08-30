02738cc0bca112eb4af67e70e986906d6742cbf1
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2016-08-30 10:29:16 +0300

    [BUGFIX] #160: remove cHash recalculation completely

--- Classes/Configuration/ConfigurationReader.php
+++ Classes/Configuration/ConfigurationReader.php
@@ -87,7 +87,6 @@ class ConfigurationReader {
 		'fileName/defaultToHTMLsuffixOnPrev' => FALSE,
 		'init/appendMissingSlash' => 'ifNotFile,redirect[301]',
 		'init/emptySegmentValue' => '',
-		'init/recalculateChashIfMissing' => false,
 		'pagePath/spaceCharacter' => '-', // undocumented & deprecated!
 	);
 
