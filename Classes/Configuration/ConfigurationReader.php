8ade5c3dce380080680a6237545bb8dcb6a56896
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2016-09-09 13:01:09 +0300

    [TASK] Fix #244: restore cHash calculation as optional feature

--- Classes/Configuration/ConfigurationReader.php
+++ Classes/Configuration/ConfigurationReader.php
@@ -87,6 +87,7 @@ class ConfigurationReader {
 		'fileName/defaultToHTMLsuffixOnPrev' => FALSE,
 		'init/appendMissingSlash' => 'ifNotFile,redirect[301]',
 		'init/emptySegmentValue' => '',
+		'init/calculateChashIfMissing' => false,
 		'pagePath/spaceCharacter' => '-', // undocumented & deprecated!
 	);
 
