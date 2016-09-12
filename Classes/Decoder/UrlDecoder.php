33081443bb1406f43c3c3e6dee64808e8dea3471
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2016-09-12 12:43:59 +0300

    [TASK] Disable cHash calculation again

--- Classes/Decoder/UrlDecoder.php
+++ Classes/Decoder/UrlDecoder.php
@@ -1327,7 +1327,7 @@ class UrlDecoder extends EncodeDecoderBase implements SingletonInterface {
 		$this->checkExpiration($cacheEntry);
 
 		if ($calculateChash) {
-			$this->calculateChash($cacheEntry);
+//			$this->calculateChash($cacheEntry);
 		}
 
 		$this->setRequestVariables($cacheEntry);
