d4de3d53c8b4fc4aa0f5cdf327ae8cd7893821b7
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2016-09-14 16:58:19 +0300

    [TASK] Fix #219: replace also non-breaking space and tab in convertToSafeString()

--- Classes/Utility.php
+++ Classes/Utility.php
@@ -96,7 +96,7 @@ class Utility {
 			$processedTitle = $this->csConvertor->conv_case('utf-8', $processedTitle, 'toLower');
 		}
 		$processedTitle = strip_tags($processedTitle);
-		$processedTitle = preg_replace('/[ \-+_]+/', $spaceCharacter, $processedTitle);
+		$processedTitle = preg_replace('/[\s\x{00A0}\-+_]+/', $spaceCharacter, $processedTitle);
 		$processedTitle = $this->csConvertor->specCharsToASCII('utf-8', $processedTitle);
 		$processedTitle = preg_replace('/[^\p{L}0-9' . preg_quote($spaceCharacter) . ']/u', '', $processedTitle);
 		$processedTitle = preg_replace('/' . preg_quote($spaceCharacter) . '{2,}/', $spaceCharacter, $processedTitle);
