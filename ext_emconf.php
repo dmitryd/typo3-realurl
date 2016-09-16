bb4b29273a7172a6976520e452dedbf3ae7983ad
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2016-09-16 16:21:47 +0300

    [TASK] Require PHP at least 5.4.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,7 +26,7 @@ $EM_CONF[$_EXTKEY] = array (
     'depends' => 
     array (
       'typo3' => '6.2.0-8.0.999',
-      'php' => '5.3.2-7.0.999',
+      'php' => '5.4.0-7.0.999',
       'scheduler' => '6.2.0-8.0.999',
     ),
     'conflicts' => 
