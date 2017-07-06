71d2a169cd744256999ff06e229fa5339e5767c6
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2017-07-06 17:26:27 +0300

    [TASK] Fix #59: add some useful logging

--- Classes/EncodeDecoderBase.php
+++ Classes/EncodeDecoderBase.php
@@ -30,6 +30,7 @@
 namespace DmitryDulepov\Realurl;
 
 use DmitryDulepov\Realurl\Cache\CacheInterface;
+use TYPO3\CMS\Core\Log\LogManager;
 use TYPO3\CMS\Core\Utility\ArrayUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
@@ -57,6 +58,9 @@ abstract class EncodeDecoderBase {
 
 	/** @var array */
 	protected $ignoredUrlParameters = array();
+
+	/** @var \TYPO3\CMS\Core\Log\Logger */
+	protected $logger;
 
 	/** @var PageRepository */
 	protected $pageRepository = NULL;
@@ -86,6 +90,7 @@ abstract class EncodeDecoderBase {
 	public function __construct() {
 		Utility::checkAndPerformRequiredUpdates();
 		$this->databaseConnection = $GLOBALS['TYPO3_DB'];
+		$this->logger = GeneralUtility::makeInstance(LogManager::class)->getLogger(get_class($this));
 		$this->tsfe = $GLOBALS['TSFE'];
 		// Warning! It is important to init the new object and not reuse any existing object
 		// $this->pageRepository->sys_language_uid must stay 0 because we will do overlays manually
