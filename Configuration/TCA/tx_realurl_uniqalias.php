b9699f28c750204cbc39351c77a627f45d6fadba
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2017-10-05 11:04:53 +0300

    [TASK] Add labels for realurl tables to prevent various warnings

--- Configuration/TCA/tx_realurl_uniqalias.php
+++ Configuration/TCA/tx_realurl_uniqalias.php
@@ -24,12 +24,14 @@
 
 $GLOBALS['TCA']['tx_realurl_uniqalias'] = array(
 	'ctrl' => array(
+		'title' => 'LLL:EXT:realurl/Resources/Private/Language/locallang_db.xlf:tx_realurl_uniqalias',
 		'label' => 'value_alias',
+		'iconfile' => 'EXT:realurl/Resources/Public/Icons/Extension.svg',
 		'hideTable' => 1,
 	),
 	'columns' => array(
 		'expire' => array(
-			'label' => '',
+			'label' => 'LLL:EXT:realurl/Resources/Private/Language/locallang_db.xlf:tx_realurl_uniqalias.expire',
 			'config' => array(
 				'type' => 'input',
 				'renderType' => 'inputDateTime',
@@ -38,7 +40,7 @@ $GLOBALS['TCA']['tx_realurl_uniqalias'] = array(
 			)
 		),
 		'lang' => array(
-			'label' => '',
+			'label' => 'LLL:EXT:realurl/Resources/Private/Language/locallang_db.xlf:tx_realurl_uniqalias.lang',
 			'config' => array(
 				'type' => 'input',
 				'eval' => 'int,required',
@@ -46,39 +48,46 @@ $GLOBALS['TCA']['tx_realurl_uniqalias'] = array(
 			)
 		),
 		'tablename' => array(
-			'label' => '',
+			'label' => 'LLL:EXT:realurl/Resources/Private/Language/locallang_db.xlf:tx_realurl_uniqalias.tablename',
 			'config' => array(
 				'type' => 'input',
 				'eval' => 'required',
 			)
 		),
 		'value_alias' => array(
-			'label' => '',
+			'label' => 'LLL:EXT:realurl/Resources/Private/Language/locallang_db.xlf:tx_realurl_uniqalias.value_alias',
 			'config' => array(
 				'type' => 'input',
 				'eval' => 'required',
 			)
 		),
 		'value_id' => array(
-			'label' => '',
+			'label' => 'LLL:EXT:realurl/Resources/Private/Language/locallang_db.xlf:tx_realurl_uniqalias.value_id',
 			'config' => array(
 				'type' => 'input',
 				'eval' => 'int,required',
 			)
 		),
 		'field_alias' => array(
-			'label' => '',
+			'label' => 'LLL:EXT:realurl/Resources/Private/Language/locallang_db.xlf:tx_realurl_uniqalias.field_alias',
 			'config' => array(
 				'type' => 'input',
 				'eval' => 'required',
 			)
 		),
 		'field_id' => array(
-			'label' => '',
+			'label' => 'LLL:EXT:realurl/Resources/Private/Language/locallang_db.xlf:tx_realurl_uniqalias.field_id',
 			'config' => array(
 				'type' => 'input',
 				'eval' => 'required',
 			)
 		),
-	)
+	),
+	'types' => array(
+		'types' => array(
+			0 => array(
+				'showitem' => '',
+			),
+		),
+	),
 );
