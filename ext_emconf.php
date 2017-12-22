739ee90198ba07120e65c58b40ca891ec8648b03
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2017-12-22 13:57:57 +0300

    [TASK] Enable testing on TYPO3 9.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,9 +25,9 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' => 
     array (
-      'typo3' => '6.2.0-8.7.999',
+      'typo3' => '6.2.0-9.0.999',
       'php' => '5.4.0-7.1.999',
-      'scheduler' => '6.2.0-8.7.999',
+      'scheduler' => '6.2.0-9.0.999',
     ),
     'conflicts' => 
     array (
