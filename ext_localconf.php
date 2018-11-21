7c79e7dfc356b7e8c18880655f2df084d2304bc5
mabolek <mathias@lilio.com>
2018-11-21 15:48:59 +0100

    [BUGFIX] Changed GeneralUtility::requireOnce() to require_once()

--- ext_localconf.php
+++ ext_localconf.php
@@ -29,11 +29,11 @@ if (!function_exists('includeRealurlConfiguration')) {
 		$realurlConfigurationFile = trim($configuration['configFile']);
 		if ($realurlConfigurationFile && @file_exists(PATH_site . $realurlConfigurationFile)) {
 			/** @noinspection PhpIncludeInspection */
-			\TYPO3\CMS\Core\Utility\GeneralUtility::requireOnce(PATH_site . $realurlConfigurationFile);
+			require_once(PATH_site . $realurlConfigurationFile);
 		}
 		elseif ($configuration['enableAutoConf'] && file_exists(PATH_site . TX_REALURL_AUTOCONF_FILE)) {
 			/** @noinspection PhpIncludeInspection */
-			\TYPO3\CMS\Core\Utility\GeneralUtility::requireOnce(PATH_site . TX_REALURL_AUTOCONF_FILE);
+            require_once(PATH_site . TX_REALURL_AUTOCONF_FILE);
 		}
 
 		if (is_array($existingConfiguration)) {
