59ba5bec5761b87f50488fd12021b07988d0c8ee
Daniel Zabel <daniel.zabel@bitmarck.de>
2021-10-31 16:53:34 +0100

    php 7.4 compatibility
    - fixes "Array and string offset access syntax with curly braces is deprecated"

--- Classes/Configuration/ConfigurationReader.php
+++ Classes/Configuration/ConfigurationReader.php
@@ -234,7 +234,7 @@ class ConfigurationReader {
 		$globalConfig = &$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl'];
 		foreach ($globalConfig['_DOMAINS']['decode'] as $domainName => $configuration) {
 			$checkThisConfiguration = false;
-			if ($domainName{0} == '/') {
+			if ($domainName[0] == '/') {
 				// Regular expression, match only main host name
 				if (@preg_match($domainName, $this->hostName)) {
 					$checkThisConfiguration = true;
